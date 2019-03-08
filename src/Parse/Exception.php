ca593dd2d60720506f03eb30ff76349f0730429d
Alexander Letnikow <reclamme@gmail.com>
2019-03-08 21:21:48 +0200

    Add base parse exception interface

---
+++ src/Parse/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Horat1us\SimpleXML\Parse;
+
+/**
+ * Interface Exception
+ * Common exception interface for parsing errors.
+ * @package Horat1us\SimpleXML\Parse
+ */
+interface Exception extends \Throwable
+{
+}
